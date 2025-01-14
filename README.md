18dff7a0ed5502f7fe55b1afe7fb18d15eda83cc
Yann <yann.ylc@orange.fr>
2025-01-13 21:29:13 -0500

    version 2025. Voir readme pour les problèmes à corriger

--- README.md
+++ README.md
@@ -12,3 +12,14 @@ Filtre pour Wireshark :
 ```
 udp.port==5005 || tcp.port==8888
 ```
+
+Problèmes de Janvier 2025 à régler:
+- Les extensions de VSCode ne permettent pas de déboguer du Python2. Les applications sur la MPIC DOIVENT rouler en Python2.
+  On les lance depuis le terminal, car même le "start" dans VSCode ne veut pas utiliser Python2.
+  En Python3, on a un segfault avec le 429 et des cast qui ne fonctionnent pas (code à refactorer).
+- Pareil, la dernière extension VSCode ne permet pas de démarrer laptop.py en mode débogue avec Python 3.7.8,
+  version qui fonctionne.
+- laptop.py ne fonctionne pas avec Pyhon 3.13.1, il bloque / n'accepte pas la connexion tcp.
+- Même chose avec 3.8.10, version minimale pour faire du debug.
+- On doit donc lancer avec 3.7.8 et pas en mode debug. D'où l'ajout des "input" dans le code pour
+  y aller step by step sans debug
--- laptop.py
+++ laptop.py
@@ -1,37 +1,49 @@
 import socket
 
 if __name__ == '__main__':
+
+    input("Avant connexion UDP. Appuyez sur Entrée pour continuer...")
+    
     # Etablissement d'une connexion UDP : socket(), bind()
     socket_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     socket_udp.bind(("0.0.0.0", 5005))
+    
+    input("Avant connexion TCP. Appuyez sur Entrée pour continuer...")
     
     # Etablissement d'une connexion TCP : socket(), bind(), listen(), accept()
     socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket_tcp.bind(("0.0.0.0", 8888))
     socket_tcp.listen(1)
     conn_tcp, addr = socket_tcp.accept()
-
+    
+    input("Avant envoi TCP. Appuyez sur Entrée pour continuer...")
+    
     # Envoi d'un message TCP a la MPIC :
     # Simule une application CAE (Windows + Ethernet) qui veut communiquer avec un instrument CAN bus (A350 KCCU)
     # On utilise la MPIC comme Interface (Ethernet <-> CAN bus)
     conn_tcp.send(b"canbus")
+    
+    input("Avant reception UDP. Appuyez sur Entrée pour continuer...")
     
     # Attente d'un message UDP de l'instrument (de la MPIC qui le remplace) a la reception du message CAN bus
     # (Non realiste : l'instrument n'est pas connecté en UDP en vrai - c'est pour l'exemple)
     data = socket_udp.recv(1024)
     print("Donnee recue :", data.decode()) # A350 KCCU a recu le message 0xaabbccddeeffaabb
     
+    input("Avant envoi TCP. Appuyez sur Entrée pour continuer...")
+    
     # Envoi d'un message TCP a la MPIC :
     # Simule une application CAE (Windows + Ethernet) qui veut communiquer avec un instrument A429 (B737 MAX FANS MCDU)    
     # On utilise la MPIC comme Interface (Ethernet <-> A429)
     conn_tcp.send(b"a429")
     
+    input("Avant reception UDP. Appuyez sur Entrée pour continuer...")
+    
     # Attente d'un message UDP de l'instrument (de la MPIC qui le remplace) a la reception du message A429
     # (Non realiste : l'instrument n'est pas connecté en UDP en vrai - c'est pour l'exemple)
     data = socket_udp.recv(1024)
     print("Donnee recue :", data.decode()) # B737 MAX FANS MCDU a recu le message 0b10010001100011000100010000001101
-
-
+    
     # Fermeture des sockets
     socket_udp.close()
     conn_tcp.close()
